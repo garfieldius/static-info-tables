b297b415af519f0e8fd6bff6ff653f421c01960f
Georg Großberger <contact@grossberger-ge.org>
2020-04-25 09:59:50 +0200

    [TASK] Add missing field to sys_language table

--- ext_tables.sql
+++ ext_tables.sql
@@ -101,3 +101,10 @@ CREATE TABLE static_territories (
 	tr_name_en varchar(50) DEFAULT '' NOT NULL,
 	PRIMARY KEY (uid)
 );
+
+#
+# Table structure for table "sys_language"
+#
+CREATE TABLE sys_language (
+    static_lang_isocode int(11) DEFAULT '0' NOT NULL
+);
